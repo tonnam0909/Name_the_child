52cbfcad91c37bcf3582aa841c3a7809aeb0d064
tonnam0909 <150112336+tonnam0909@users.noreply.github.com>
2023-12-05 21:01:01 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ with st.sidebar:
 
 def generate_name_recommendation(gender, characteristics, first_letter, language):
     # Customize the prompt based on your requirements
-    prompt = f"Give me five {gender} names that means {characteristics} that starts with {first_letter} in {language} and tell me the origin of each name."
+    prompt = f"Give me five {gender} names that means {characteristics} that starts with {first_letter} in {language} and tell me the origin of each name. Just give me the name don't write opening words"
 
     # Call OpenAI API for recommendation
     response = openai.chat.completions.create(
