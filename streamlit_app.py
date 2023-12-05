14aed4e70bf06ff275c6e85b950efbeb609bcac7
tonnam0909 <150112336+tonnam0909@users.noreply.github.com>
2023-12-05 17:19:15 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,20 @@
 import streamlit as st
+import openai  # Make sure to install the 'openai' library using: pip install openai
 
-# Function to generate a fictional name
-def generate_name(gender, characteristic, first_letter, language):
-    # Replace this with the actual OpenAI API call once available
-    # For now, a fictional name is generated based on the provided parameters
-    name = f"{first_letter.upper()}{characteristic.capitalize()}"
+# Function to generate a name using OpenAI API
+def generate_name(gender, characteristic, first_letter, language, api_key):
+    # Set OpenAI API key
+    openai.api_key = api_key
 
-    return name
+    # Call OpenAI API to generate a name based on the provided parameters
+    response = openai.Completion.create(
+        engine="text-davinci-002",  # Use the appropriate engine
+        prompt=f"Generate a {gender.lower()} name that is {characteristic.lower()} and starts with {first_letter.lower()} in {language.lower()}",
+        max_tokens=50  # Adjust as needed
+    )
+
+    generated_name = response.choices[0].text.strip()
+    return generated_name
 
 # Streamlit app
 def main():
@@ -17,10 +25,11 @@ def main():
     characteristic = st.text_input("Enter a characteristic (e.g., Brave, Intelligent)")
     first_letter = st.text_input("Enter the first letter of the name")
     language = st.selectbox("Select language", ["English", "Italian", "Spanish"])
+    api_key = st.text_input("Enter your OpenAI API key", type="password")  # Use password input for API key
 
     # Generate and display the name
     if st.button("Generate Name"):
-        generated_name = generate_name(gender, characteristic, first_letter, language)
+        generated_name = generate_name(gender, characteristic, first_letter, language, api_key)
         st.success(f"The generated name is: {generated_name}")
 
 if __name__ == "__main__":
