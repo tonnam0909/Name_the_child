50bcdbcd214d9796d7824904a25dd9316a55488d
tonnam0909 <150112336+tonnam0909@users.noreply.github.com>
2023-12-07 14:53:02 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,9 +49,7 @@ def generate_name_recommendation(gender, characteristics, first_letter, language
     return response.choices[0].message.content
 
 st.markdown("<div style='text-align: center; color: pink;'><h2 style='font-size: 2rem;'>Name your child🧒</h2></div>", unsafe_allow_html=True)
-st.markdown('''
-    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
-    :gray[pretty] :rainbow[colors].''')
+st.markdown(''':rainbow[Welcome!] Are you having trouble :red[naming] your child? Do you find it difficult to find a name that is :blue[unique] and :green[meaningful]? :rainbow[Don't worry!] We are here to help you! :rainbow[Just fill in the form below] and we will generate a list of names for you!''')
 # Uncomment the following lines to enable the API key input form
 
 
